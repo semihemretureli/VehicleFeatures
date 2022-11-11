7d5c4a037cd457a8637c56f915fdd15e4964e852
bgunduz55 <bgunduz_94@hotmail.com>
2022-11-12 01:10:37 +0300

    All Car brands and models works by json datas dynamically

--- app/src/main/java/com/semih/vehiclefeatures/CarInfo.java
+++ app/src/main/java/com/semih/vehiclefeatures/CarInfo.java
@@ -5,10 +5,33 @@ import java.io.Serializable;
 public class CarInfo implements Serializable {
     private String brand;
     private String model;
+    private String motorType;
+    private String maxSpeed;
+    private String zeroToHundredSpeed;
+    private String gearType;
+    private String wheelSize;
+    private String bodyType;
+    private String energyType;
+    private double bodyWeight;
+    private String productionYears;
+    private int euroNcapPoint;
+    private String pullType;
 
-    public CarInfo(String brand, String model) {
+
+    public CarInfo(String brand, String model, String motorType, String maxSpeed, String zeroToHundredSpeed, String gearType, String wheelSize, String bodyType, String energyType, double bodyWeight, String productionYears, int euroNcapPoint, String pullType) {
         this.brand = brand;
         this.model = model;
+        this.motorType = motorType;
+        this.maxSpeed = maxSpeed;
+        this.zeroToHundredSpeed = zeroToHundredSpeed;
+        this.gearType = gearType;
+        this.wheelSize = wheelSize;
+        this.bodyType = bodyType;
+        this.energyType = energyType;
+        this.bodyWeight = bodyWeight;
+        this.productionYears = productionYears;
+        this.euroNcapPoint = euroNcapPoint;
+        this.pullType = pullType;
     }
 
     public CarInfo() {
@@ -30,11 +53,110 @@ public class CarInfo implements Serializable {
         this.model = model;
     }
 
+    public String getMotorType() {
+        return motorType;
+    }
+
+    public void setMotorType(String motorType) {
+        this.motorType = motorType;
+    }
+
+    public String getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(String maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getZeroToHundredSpeed() {
+        return zeroToHundredSpeed;
+    }
+
+    public void setZeroToHundredSpeed(String zeroToHundredSpeed) {
+        this.zeroToHundredSpeed = zeroToHundredSpeed;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getWheelSize() {
+        return wheelSize;
+    }
+
+    public void setWheelSize(String wheelSize) {
+        this.wheelSize = wheelSize;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public String getEnergyType() {
+        return energyType;
+    }
+
+    public void setEnergyType(String energyType) {
+        this.energyType = energyType;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public String getProductionYears() {
+        return productionYears;
+    }
+
+    public void setProductionYears(String productionYears) {
+        this.productionYears = productionYears;
+    }
+
+    public int getEuroNcapPoint() {
+        return euroNcapPoint;
+    }
+
+    public void setEuroNcapPoint(int euroNcapPoint) {
+        this.euroNcapPoint = euroNcapPoint;
+    }
+
+    public String getPullType() {
+        return pullType;
+    }
+
+    public void setPullType(String pullType) {
+        this.pullType = pullType;
+    }
+
     @Override
     public String toString() {
         return "CarInfo{" +
-                "marka='" + brand + '\'' +
+                "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
+                ", motorType='" + motorType + '\'' +
+                ", maxSpeed='" + maxSpeed + '\'' +
+                ", zeroToHundredSpeed='" + zeroToHundredSpeed + '\'' +
+                ", gearType='" + gearType + '\'' +
+                ", wheelSize='" + wheelSize + '\'' +
+                ", bodyType='" + bodyType + '\'' +
+                ", energyType='" + energyType + '\'' +
+                ", bodyWeight=" + bodyWeight +
+                ", productionYears='" + productionYears + '\'' +
+                ", euroNcapPoint=" + euroNcapPoint +
+                ", pullType='" + pullType + '\'' +
                 '}';
     }
 }
--- app/src/main/java/com/semih/vehiclefeatures/MainActivity.java
+++ app/src/main/java/com/semih/vehiclefeatures/MainActivity.java
@@ -2,6 +2,8 @@ package com.semih.vehiclefeatures;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,8 @@ import android.widget.Button;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.semih.vehiclefeatures.adapters.CarBrandAdapter;
+import com.semih.vehiclefeatures.databinding.ActivityMainBinding;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -18,61 +22,35 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
-//    private Button mercedes;
-//    private Button citroen;
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding binding;
     CarInfoList cars;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //mercedes = (Button) findViewById(R.id.btnMercedes);
-        //citroen = (Button) findViewById(R.id.btnCitroen);
-        cars=getAllCars();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        cars = getAllCars();
+        HashMap<String, CarInfo> carMap = new HashMap<>();
+        for (CarInfo item: cars.getCarList()) {
+            carMap.put(item.getBrand(), item);
+        }
+        CarBrandAdapter adapter = new CarBrandAdapter(new ArrayList<CarInfo>(carMap.values()), this, false);
+
+        binding.rvCarBrand.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(MainActivity.this);
+        binding.rvCarBrand.setLayoutManager(layoutManager);
+        binding.rvCarBrand.setHasFixedSize(true);
+        adapter.notifyDataSetChanged();
         System.out.println(cars.toString());
-       /* mercedes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent btnMercedes = new Intent(MainActivity.this, MercedesModel.class);
-                btnMercedes.putExtra("carInfo", new CarInfo("mercedes", "190E"));
-                startActivity(btnMercedes);
-            }
-        });
-        citroen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent btnCitroen = new Intent(MainActivity.this, CModel.class);
-                btnCitroen.putExtra("carInfo", new CarInfo("citroen", "c5AirCross"));
-                startActivity(btnCitroen);
-            }
-        });*/
+
     }
 
-    private CarInfoList getAllCars() {
-        InputStream is = getResources().openRawResource(R.raw.cars);
-        Writer writer = new StringWriter();
-        char[] buffer = new char[1024];
-        try {
-            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            int n;
-            while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
 
-            }
-            is.close();
-        } catch (Exception e){
-
-        }
-            finally {
-            //is.close();
-        }
-
-        String jsonString = writer.toString();
-        Gson gson=new GsonBuilder().create();
-       CarInfoList cars1 = gson.fromJson(jsonString,CarInfoList.class);
-       return cars1;
-    }
 }
